d7cd02df2c470d69c3598bb495954892a47d3fc3
Yannis Arnouts <yannis.arnouts@student.kdg.be>
2018-06-11 18:44:51 +0200

    detail

---
+++ app/src/main/java/com/example/yannis/trainfriendsfinder/Detail.java
@@ -0,0 +1,50 @@
+package com.example.yannis.trainfriendsfinder;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Detail extends android.app.Fragment {
+    TextView tv_naam;
+    public static String naamWaarde;
+
+    public Detail() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static Detail newInstance(String naam, String tijd) {
+        Detail fragment = new Detail();
+        naamWaarde = naam;
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        tv_naam = view.findViewById(R.id.tv_naam);
+
+        updateViews(naamWaarde);
+        return view;
+    }
+
+    public void updateViews(String name) {
+        tv_naam.setText(name);
+    }
+
+}
